f98fd858401ca64a5356abd6baff96fd7d9563f5
Thomas Lynn <thomaslynn01@gmail.com>
2021-03-02 02:48:12 +0000

    added agent rigidbody

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -9,6 +9,7 @@ public class Agent : Unity.MLAgents.Agent
 {
 
     public Transform wheel;
+    public Rigidbody body;
 
     private Vector3 lastPos = Vector3.zero;
     // Start is called before the first frame update
@@ -23,14 +24,22 @@ public class Agent : Unity.MLAgents.Agent
         //transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
         Debug.DrawLine(transform.position, transform.position + transform.forward);
         wheel.Rotate(0, transform.InverseTransformVector(transform.position - lastPos).z * -60, 0);
-        print((transform.position - lastPos).magnitude + " " + transform.position);
         lastPos = transform.position;
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        transform.Translate(Mathf.Clamp(actionBuffers.ContinuousActions[0],-1f,1f) * Time.fixedDeltaTime, 0, Mathf.Clamp(actionBuffers.ContinuousActions[1], -1f, 1f) * Time.fixedDeltaTime, Space.Self);
-        transform.Rotate(0, Mathf.Clamp(actionBuffers.ContinuousActions[2], -1f, 1f) * Time.fixedDeltaTime * 45, 0);
+        Vector3 move = actionBuffers.ContinuousActions[0] * transform.right + actionBuffers.ContinuousActions[1] * transform.forward;
+        if (move.magnitude > 1)
+        {
+            move = move.normalized;
+        }
+        body.MovePosition( body.position + move * Time.fixedDeltaTime);
+        Quaternion newAngles = body.rotation;
+        Vector3 euler = newAngles.eulerAngles;
+        euler.y += Mathf.Clamp(actionBuffers.ContinuousActions[2], -1f, 1f) * Time.fixedDeltaTime * 45;
+        newAngles.eulerAngles = euler;
+        body.MoveRotation(newAngles);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
