4453b1cb0124769dfb71d444ecd9d4c2740267a9
Thomas Lynn <thomaslynn01@gmail.com>
2021-03-02 01:12:10 +0000

    Added bot model + agent

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class Agent : MonoBehaviour
+public class Agent : Unity.MLAgents.Agent
 {
+
+    public Transform wheel;
+
+    private Vector3 lastPos = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +20,59 @@ public class Agent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
+        //transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
         Debug.DrawLine(transform.position, transform.position + transform.forward);
+        wheel.Rotate(0, transform.InverseTransformVector(transform.position - lastPos).z * -60, 0);
+        print((transform.position - lastPos).magnitude + " " + transform.position);
+        lastPos = transform.position;
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        transform.Translate(Mathf.Clamp(actionBuffers.ContinuousActions[0],-1f,1f) * Time.fixedDeltaTime, 0, Mathf.Clamp(actionBuffers.ContinuousActions[1], -1f, 1f) * Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(0, Mathf.Clamp(actionBuffers.ContinuousActions[2], -1f, 1f) * Time.fixedDeltaTime * 45, 0);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            actionsOut.ContinuousActions.Array[1] = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            actionsOut.ContinuousActions.Array[1] = -1f;
+        }
+        else
+        {
+            actionsOut.ContinuousActions.Array[1] = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            actionsOut.ContinuousActions.Array[0] = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            actionsOut.ContinuousActions.Array[0] = -1f;
+        }
+        else
+        {
+            actionsOut.ContinuousActions.Array[0] = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            actionsOut.ContinuousActions.Array[2] = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            actionsOut.ContinuousActions.Array[2] = -1f;
+        }
+        else
+        {
+            actionsOut.ContinuousActions.Array[2] = 0f;
+        }
     }
 }
