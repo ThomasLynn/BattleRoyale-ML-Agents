19542a7ba64979616c7641c2041ffa7374279927
Thomas Lynn <thomaslynn01@gmail.com>
2021-03-12 20:52:10 +0000

    fixed ui

--- Assets/Scripts/TextScript.cs
+++ Assets/Scripts/TextScript.cs
@@ -9,10 +9,13 @@ public class TextScript : MonoBehaviour
     public Agent targetAgent;
     public string startingText;
     public Text text;
+    public RectTransform trans;
+    public Vector2 offset;
 
     // Update is called once per frame
     void Update()
     {
         text.text = startingText + targetAgent.points;
+        trans.anchoredPosition = new Vector2(-Screen.width / 2, Screen.height / 2) + offset;
     }
 }
